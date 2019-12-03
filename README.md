f65d85c3bf1d69b04ab5135944a911c1421f58ef
Eric F. Romanenghi <ericromanenghi@gmail.com>
2019-12-03 23:46:34 +0100

    Add support for categories
    Also, some refactoring was done to extract code from pages and move
    it to components under _include so they could be reused.

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ TODO:
    * ~~Short explanation about the different categories in the website (it will keep expanding while adding new content, I can start just with poems and short stories)~~
    * ~~Short "about me" section~~
 * ~~License content as creative commons~~
+* ~~Add pages for categories~~
 * Add Google Analytics support
 * Start adding content!
 * Add support for English, translate at least the main page (translate content itself will be way harder but I still have some hopes I will eventually do it)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-});
+function initSidenav() {
+    document.addEventListener('DOMContentLoaded', function() {
+        let elems = document.querySelectorAll('.sidenav');
+        let instances = M.Sidenav.init(elems);
+    });
+}
+
+function initFlowtext() {
+    let nodes = Array.from(document.getElementsByTagName('p'));
+    nodes.map(e => e.classList.add('flow-text'));
+}
+
+initSidenav();
+initFlowtext();
